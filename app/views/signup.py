eb610163c409994aedf70861ace49a4e852375cb
Enias Oliveira <eniasoliveira27@gmail.com>
2021-03-16 18:39:47 -0300

    feat: adds user creation to signup route when receiving post

--- app/views/signup.py
+++ app/views/signup.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
-from flask import Blueprint
+from flask import Blueprint, request
+from http import HTTPStatus
+from environs import Env
+
+from app.models.users_model import UsersModel
+
 
 signup = Blueprint("signup", __name__)
+
+env = Env()
+env.read_env()
+
+FILENAME = "data/users.csv" if env("TEST") != "true" else "data/users_test.csv"
 
 
 @signup.route("/signup", methods=["POST"])
 def signup_index():
-    return "Batata"
+    request_user = request.get_json()
+
+    users = UsersModel(FILENAME)
+    created_user = users.signup(request_user)
+
+    return created_user, HTTPStatus.CREATED
